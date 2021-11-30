71716612cd572768e18bc8114c4a9c8667212e3e
Juan Romero <juan.romero@serviefectivo.com.co>
2021-11-30 10:19:20 -0500

    Generalidades

---
+++ resources/js/components/blog/Crear.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Crear Blog</h4>
+                    </div>
+                    <div class="card-body">
+                        <form @submit.prevent="crear">
+                            <div class="form-group row">
+                                <label for="text" class="col-4 col-form-label">Título</label> 
+                                <div class="col-8">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                    <div class="input-group-text">
+                                        <i class="fa fa-address-card"></i>
+                                    </div>
+                                    </div> 
+                                    <input v-model="blog.titulo" id="text" name="text" type="text" class="form-control" >
+                                </div>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="textarea" class="col-4 col-form-label">Contenido</label> 
+                                <div class="col-8">
+                                <textarea v-model="blog.contenido" id="textarea" name="textarea" cols="40" rows="5" class="form-control"></textarea>
+                                </div>
+                            </div> 
+                            <div class="form-group row">
+                                <div class="offset-4 col-8">
+                                <button name="submit" type="submit" class="btn btn-primary">Guardar</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"crear-blog",
+    data(){
+        return{
+            blog:{
+                titulo:"",
+                contenido:""
+            }
+        }
+    },
+    methods:{
+        async crear(){
+            await this.axios.post('/api/blog', this.blog)
+            .then(response=>{
+                this.$router.push({name:"mostrarBlogs"})
+
+            })
+            .catch(error=>{
+               console.log(error) 
+            })
+        }
+    }
+}
+</script>
---
+++ resources/js/components/blog/Mostrar.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="row">
+        <div class="col-lg-12 mb-4">
+            <router-link :to='{name:"crearBlog"}' class="btn btn-success"><i class="fas fa-plus-circle"></i> Nuevo</router-link>
+        </div>
+        <div class="col-12">
+        <div class="table-responsive">
+            <table class="table table-bordered">
+                <thead  class="bg-primary text-white">
+                    <th>ID</th>
+                    <th>Título</th>
+                    <th>Contenido</th>
+                    <th>Acciones</th>
+                </thead>
+                <tbody>
+                    <tr v-for="blog in blogs" :key="blog.id">
+                        <td>{{blog.id}}</td>
+                        <td>{{blog.titulo}}</td>
+                        <td>{{blog.contenido}}</td>
+                        <td>
+                            <router-link :to='{name:"editarBlog", params: {id:blog.id}}' class="btn btn-info"><i class="far fa-edit"> Editar</i></router-link>
+                            <a type="button" @click="borrarBlog(blog.id)" class="btn btn-danger"><i class="fas fa-trash"> Borrar</i></a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name:"blogs",
+    data(){
+        return{
+            blogs:[]
+        }
+    },
+    mounted(){
+        this.mostrarBlogs()
+    },
+    methods:{
+        async mostrarBlogs(){
+            await this.axios.get('api/blog')
+            .then(response=>{
+                this.blogs = response.data
+            })
+            .catch(error=>{
+                this.blog = []
+            })
+        },
+        borrarBlog(id){
+            if(comfirm("¿Confirma eliminar el registro?")){
+                this.axios.delete(`/api/blog/${id}`)
+                .then(response=>{
+                    this.blogs = mostarBlogs()
+                })
+                .catch(error=>{
+                    console.log(error) 
+                })
+            }
+        }
+    }
+}
+</script>
